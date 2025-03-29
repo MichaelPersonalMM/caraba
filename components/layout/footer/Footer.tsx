8e648b89486e4b70d9cbdb2e8ac64fe5541c6ab4
MichaelPersonalMM <michael.mccullough@gmail.com>
2025-03-30 00:38:39 +0800

    Update Footer.tsx
    &copy; {new Date().getFullYear()}

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="absolute bottom-0 w-full border-t border-primary border-opacity-10 bg-gray-100">
       <div className="container mx-auto px-4 py-6 text-center text-sm text-primary">
-        © 2024 Carabao Capital. All rights reserved.
+        &copy; {new Date().getFullYear()} Carabao Capital. All rights reserved.
       </div>
     </footer>
   );
